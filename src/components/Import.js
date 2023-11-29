bc46d314c770421180855e3d76baaa41fd87b0da
Marc Rozenburgh <znep27@hotmail.com>
2023-11-28 20:51:22 -0600

    update

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import { Button, Paper, Table, TableContainer, TableHead, TableBody, TableRow, TableCell } from '@mui/material'
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import IconButton from "@mui/material/IconButton";
 
 const Import = (props) => {
     // fill out this component
+    // <Button variant="contained" color="primary" className={classes.button}>
+    //   Import
+    // </Button>
+
 
     return (
-        <p>Import Component</p>
+        // <p>Import Component</p>
+        <TableContainer component={Paper}>
+            <h2>Count:{props.makes.length}</h2>
+            <p>Import Component</p>
+            <Button variant="contained" onClick={props.fetchMakes}>
+                Import
+            </Button>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Make</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {props.makes.map((row, index) => (
+                    <TableRow
+                    key={index}
+                    //   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                    <TableCell>{row.MakeId}</TableCell>
+                    <TableCell>{row.MakeName}</TableCell>
+                    <TableCell>
+                        <IconButton
+                        onClick={() => {
+                            props.deleteMake(index);
+                        }}
+                        >
+                        <MoreVertIcon />
+                        </IconButton>
+                    </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,31 @@
 // fill out this container
+import { connect } from "react-redux";
+import Import from "../components/Import";
+import { fetchMakes, deleteMake } from "../redux/actions";
+
+/*
+ * Now we are going to write a `mapDispatchToProps` function. Remember, it takes one argument called "dispatch" and it returns an object whose keys are the props you want to pass. For us we will create a key called "addCar" and we'll set its value to a function that dispatches our "addCar" action creator. It looks like this: `(car) => dispatch(addCar(car))`. The car on the left will be passed from our component when we call this function. It will then be passed onto the inner "addCar" function.
+
+
+const mapStateToProps = (state) => {
+  return {
+    cars: state.cars,
+  };
+};
+
+ */
+
+const mapStateToProps = (state) => {
+  return {
+    makes: state.makes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchMakes: (index) => dispatch(fetchMakes(index)),
+    deleteMake: (index) => dispatch(deleteMake(index)),
+  };
+};
+// note currying syntax - also called partial application
+export default connect(mapStateToProps, mapDispatchToProps)(Import);

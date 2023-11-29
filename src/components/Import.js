84ffb6d576bf53407d893b32dd6c5a9b28ccd11a
Marc Rozenburgh <znep27@hotmail.com>
2023-11-28 20:58:52 -0600

    update

--- src/components/Import.js
+++ src/components/Import.js
@@ -20,31 +20,31 @@ const Import = (props) => {
             </Button>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
-                <TableRow>
-                    <TableCell>Id</TableCell>
-                    <TableCell>Make</TableCell>
-                    <TableCell>Actions</TableCell>
-                </TableRow>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
-                {props.makes.map((row, index) => (
-                    <TableRow
-                    key={index}
-                    //   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                    <TableCell>{row.MakeId}</TableCell>
-                    <TableCell>{row.MakeName}</TableCell>
-                    <TableCell>
-                        <IconButton
-                        onClick={() => {
-                            props.deleteMake(index);
-                        }}
+                    { props.makes.map((row, index) => (
+                        <TableRow
+                        key={index}
+                        //   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                         >
-                        <MoreVertIcon />
-                        </IconButton>
-                    </TableCell>
-                    </TableRow>
-                ))}
+                            <TableCell>{row.MakeId}</TableCell>
+                            <TableCell>{row.MakeName}</TableCell>
+                            <TableCell>
+                                <IconButton
+                                onClick={() => {
+                                    props.deleteMake(index);
+                                }}
+                                >
+                                    <MoreVertIcon />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    )) }
                 </TableBody>
             </Table>
         </TableContainer>
